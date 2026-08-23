d3106f2b3eedde5da0696aaad5572001791a9ceb
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-08-23 16:48:47 +0200

    initial commit
    Account and SavingsAccount classes

---
+++ src/main/java/com/josedev/Account.java
@@ -0,0 +1,52 @@
+package com.josedev;
+
+public class Account {
+    protected float balance;
+    protected int numDeposits;
+    protected int numWithdrawals;
+    protected float annualInterestRate;
+    protected float monthlyFee;
+
+    public Account(float balance, float annualInterestRate) {
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+        this.numDeposits = 0;
+        this.numWithdrawals = 0;
+        this.monthlyFee = 0;
+    }
+
+    public void deposit(float amount) {
+        this.balance += amount;
+        this.numDeposits++;
+    }
+
+    public void withdraw(float amount) {
+        if (amount > this.balance) {
+            System.out.println("Error: withdrawal amount exceeds balance");
+            return;
+        }
+        this.balance -= amount;
+        this.numWithdrawals++;
+    }
+
+    public float calculateMonthlyInterest() {
+        float monthlyRate = this.annualInterestRate / 12 / 100;
+        float interest = this.balance * monthlyRate;
+        this.balance += interest;
+        return interest;
+    }
+
+    public void monthlyStatement() {
+        this.balance -= this.monthlyFee;
+        calculateMonthlyInterest();
+    }
+
+    public String print() {
+        return
+            "Balance: " + this.balance +
+            ", Deposits: " + this.numDeposits +
+            ", Withdrawals: " + this.numWithdrawals +
+            ", Annual interest Rate: " + this.annualInterestRate +
+            ", Monthly Fee: " + this.monthlyFee;
+    }
+}
---
+++ src/main/java/com/josedev/SavingsAccount.java
@@ -0,0 +1,52 @@
+package com.josedev;
+
+public class SavingsAccount extends Account {
+    private boolean active;
+    
+    public SavingsAccount(float balance, float annualInterestRate) {
+        super(balance, annualInterestRate);
+        this.active = balance >= 10000;
+    }
+
+    @Override
+    public void deposit(float amount) {
+        if (!this.active) {
+            System.out.println("Error: account is inactive");
+            return;
+        }
+        super.deposit(amount);
+    }
+
+    @Override
+    public void withdraw(float amount) {
+        if (!this.active) {
+            System.out.println("Error: account is inactive");
+            return;
+        }
+        super.withdraw(amount);
+    }
+
+    @Override
+    public void monthlyStatement() {
+        if (this.numWithdrawals > 4) {
+            int extraWithdrawals = this.numWithdrawals - 4;
+            this.monthlyFee += extraWithdrawals * 1000;
+        }
+
+        super.monthlyStatement();
+
+        this.active = this.balance >= 10000;
+    }
+
+    public String print() {
+        int totalTransactions = this.numDeposits + this.numWithdrawals;
+        return
+        "Balance: " + this.balance +
+        ", Monthly Fee: " + this.monthlyFee +
+        ", Total Transactions: " + totalTransactions;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+}
